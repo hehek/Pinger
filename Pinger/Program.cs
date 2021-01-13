da6c1be312987ded1ecbbbfb64989db49a9c5bf9
Anna Pavlovskaya <annvsann@gmail.com>
2021-01-13 17:28:28 +0200

    Config

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Hosting;
-
 using System;
 using System.Collections.Generic;
 using System.IO;
-using Microsoft.AspNetCore.Hosting.Internal;
+
 
 namespace Pinger
 {
@@ -14,31 +12,31 @@ namespace Pinger
     {
         static void Main(string[] args)
         {
+            
             CreateHostBuilder(args).Build().Run();
-            ILoggerFactory loggerFactory = new LoggerFactory();                                              
-            ILogger logger = loggerFactory.CreateLogger<Program>();
+            ILoggerFactory loggerFactory = new LoggerFactory();
+             ILogger logger = loggerFactory.CreateLogger<Program>();
+
             var configuration = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
+                       .SetBasePath(Directory.GetCurrentDirectory())
                         .AddJsonFile("Settings.json")
-                        .Build();          
+                       .Build();
             var hostList = configuration.GetSection("Hosts").Get<List<PingerSettings>>();
 
-            foreach(var hl in hostList)
+            foreach (var hl in hostList)
             {
-                logger.LogInformation(hl.Host+"\n"
-                                           +hl.Protocol+"\n"
-                                           +hl.Status+"\n"
-                                           +hl.Timeout,"arg");                
-            }            
+                logger.LogInformation(hl.Host + "\n"
+                                           + hl.Protocol + "\n"
+                                           + hl.Status + "\n"
+                                           + hl.Timeout, "arg");
+
+            }
             Console.ReadLine();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-       Host.CreateDefaultBuilder(args)
-           .ConfigureWebHostDefaults(webBuilder =>
-           {
-               webBuilder.UseStartup<Startup>();
-           });
-
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args);
+                }
     }
 }
--- Pinger/Startup.cs
+++ Pinger/Startup.cs
@@ -8,29 +8,31 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
+
 namespace Pinger
 {
-    class Startup
+    public class Startup
     {
-        public IConfiguration Configuration { get; set; }
-      
-
-        public Startup(IConfiguration configuration)
-        {            
-            Configuration = new ConfigurationBuilder()
-                     .AddJsonFile("appsettings.json")
-                     .Build();
-        }
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
-                                                            ILoggerFactory loggerFactory)
+        public IConfiguration Configuration { get; }
+        
+        public Startup()
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            Configuration = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("Settings.json")
+                       .Build(); 
+        }
+       
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();               
+            });
             loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(),
                                                         "logger.txt"));
+           
         }
-
-
 
             public void ConfigureServices(IServiceCollection services)
         {
